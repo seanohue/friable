99c8dc9f0596e44580e5bdfc9b78ee091387f48d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-05-20 13:07:08 -0400

    Integer equality func test 1 passed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,32 @@
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+
+pub mod friable {
+
+    fn equals(a: &isize, b: &isize) -> bool {
+        a == b
     }
+
+    fn is<T>(a: &T, b: &T) -> bool {
+        a as *const T == b as *const T
+    }
+
+
+    #[test]
+    fn equality() {
+        let i = 1;
+        let j = 1;
+        let expected = true;
+        let actual = equals(&i, &j);
+        assert!(actual, expected);
+    }
+
+    #[test]
+    fn inequality() {
+        let i = 1;
+        let j = 77;
+        let actual = equals(&i, &j);
+        let expected = false;
+        assert!(actual, expected);
+    }
+
 }
